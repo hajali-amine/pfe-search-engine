25fe37ff0c3d611b0a7165073904a462bbd8fc09
Hamdimahdi99 <82580314+Hamdimahdi99@users.noreply.github.com>
2022-04-21 20:55:31 +0100

    delete functions , find functions and parse matched nodes

--- orm/neo4j_oop.py
+++ orm/neo4j_oop.py
@@ -24,10 +24,44 @@ class Neo4jOOP:
             query.append(",")
         query.pop()
         query.append("}) RETURN a AS a")
-        return self.driver.session().run("".join(query)).values()
+        result =  self.driver.session().run("".join(query)).values()
+        return self.parse_match_node(result)
 
+    def parse_match_node(self, result):
+        parsedResult = []
+        for value in result:
+            parsedNode = { "id": value[0]._id ,"label": list(value[0]._labels),"properties": value[0]._properties}
+            parsedResult.append(parsedNode)
+        return parsedResult
+
+    def find_by_id(self, type, id):
+        query = [f"Match (a:{type}) WHERE ID(a) = {id} RETURN a AS a"]
+        result = self.driver.session().run("".join(query)).values()
+        return self.parse_match_node(result)[0]
+
+    def delete_one_by_id(self, type , id):
+        query = [f"Match (a:{type}) WHERE ID(a) = {id} DELETE a"]   
+        result = self.driver.session().run("".join(query)).values()
+        return result
+    
+    def delete_all(self, type, **kwargs):
+        query = [f"Match (a:{type} {{"]
+        for key, value in kwargs.items():
+            query.append(key + ":" + f"'{value}'")
+            query.append(",")
+        query.pop()
+        query.append("}) DELETE a")
+        return self.driver.session().run("".join(query)).values()
+    def count_node(self, type, **kwargs):
+        query = [f"Match (a:{type} {{"]
+        for key, value in kwargs.items():
+                query.append(key + ":" + f"'{value}'")
+                query.append(",")
+        query.pop()
+        query.append("}) RETURN count(a) AS nodes ")
+        return self.driver.session().run("".join(query)).single()["nodes"]
 
 neo = Neo4jOOP("bolt://localhost:7687",  user="neo4j", password="pwd")
-neo.add_node("person", name="hi", age="test")
-print(neo.match_node("person", name="hi", age="test"))
+print(neo.delete_all("person", name="hi", age="test"))
+
     
