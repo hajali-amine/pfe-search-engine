187b8483b31359c4179d690eb8cc49bb18390aa0
Amine Hajali <belhajali.amine@gmail.com>
2022-11-11 23:30:22 +0100

    back and front works

--- Makefile
+++ Makefile
@@ -1,3 +1,13 @@
+b=all
+
+build:
+	@if [ $b = "all" ] ; then docker build -t aminehajali/se-front ./front && docker build -t aminehajali/se-back ./back ; fi
+	@if [ ! $b = "all" ] ; then docker build -t aminehajali/se-$b ./$b ; fi
+
+push: build
+	@if [ $b = "all" ] ; then docker push aminehajali/se-front && docker push aminehajali/se-back ; fi
+	@if [ ! $b = "all" ] ; then docker push aminehajali/se-$b ; fi
+
 create-local-cluster:
 	@kind create cluster
 	@kubectl cluster-info --context kind-kind
--- back/api/api.py
+++ back/api/api.py
@@ -2,7 +2,7 @@ import os
 from urllib import request
 import dotenv
 from flask import Flask, request, jsonify
-from flask_cors import CORS
+from flask_cors import CORS, cross_origin
 
 
 from api.data_loader import DataLoader
@@ -11,10 +11,12 @@ from graph_driver.neo4j_oop import Neo4jOOP
 
 app = Flask(__name__)
 CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 neo = Neo4jOOP(os.environ["NEO4J_URL"],  user=os.environ["NEO4J_USERNAME"], password=os.environ["NEO4J_PASSWORD"])
 
 @app.route("/api", methods=["POST"])
+@cross_origin()
 def get_data_to_insert():
     data = request.get_json()
     print(data)
@@ -22,6 +24,7 @@ def get_data_to_insert():
     return "jawwek behi"
     
 @app.route("/api/<filter>/<search>", methods=["GET"])
+@cross_origin()
 def data(filter, search):
     result = DataReader.search_by_filter(neo=neo, filter=filter, search=search) if filter != "skill" else DataReader.search_by_skill(neo=neo, search=search)
     return jsonify(result)
