735602f8e992eb69f6a95c52097ed2da63971163
Amine Hajali <belhajali.amine@gmail.com>
2022-11-14 22:30:26 +0100

    Selenium runs with go in docker

---
+++ scrapper/go.mod
@@ -0,0 +1,5 @@
+module searchengine/scrapper
+
+go 1.16
+
+require github.com/tebeka/selenium v0.9.9 // indirect
---
+++ scrapper/scrapper.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/tebeka/selenium"
+	"github.com/tebeka/selenium/chrome"
+)
+
+func main() {
+    // Run Chrome browser
+    service, err := selenium.NewChromeDriverService(os.Getenv("CHROME_DRIVER_PATH"), 4444)
+    if err != nil {
+        panic(err)
+    }
+    defer service.Stop()
+
+    caps := selenium.Capabilities{}
+    caps.AddChrome(chrome.Capabilities{Args: []string{
+        "window-size=1920x1080",
+        "--no-sandbox",
+        "--disable-dev-shm-usage",
+        "disable-gpu",
+        "--headless",
+    }})
+
+    driver, err := selenium.NewRemote(caps, "")
+    if err != nil {
+        panic(err)
+    }
+    driver.Get("https://www.google.com")
+}
