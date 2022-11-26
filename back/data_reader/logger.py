740667079ae27a4ec8699aee158c2c6020efbf09
Amine Hajali <belhajali.amine@gmail.com>
2022-11-26 22:39:49 +0100

    Remove self in data reader's decorator

--- back/data_reader/logger.py
+++ back/data_reader/logger.py
@@ -4,10 +4,10 @@ from logging import Logger
 def logging(function_name):
     def log_decorator(function_to_log):
         @wraps(function_to_log)
-        def log_decorator_wrapper(self, *args, **kwargs):
+        def log_decorator_wrapper(*args, **kwargs):
             Logger.info("Begin function")
             try:
-                value = function_to_log(self, *args, **kwargs)
+                value = function_to_log(*args, **kwargs)
                 Logger.info(f"{function_name} returned", args=args, kwargs=kwargs, result=value)
             except Exception as e:
                 Logger.error(f"Exception in {function_name}", msg=str(e))
