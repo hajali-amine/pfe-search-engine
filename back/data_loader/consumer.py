9f7e81b73c4d258db6e9ec22a14e04165b4d9076
Amine Hajali <belhajali.amine@gmail.com>
2022-11-26 22:45:36 +0100

    Add message attribute to logger

--- back/data_loader/consumer.py
+++ back/data_loader/consumer.py
@@ -13,9 +13,9 @@ try:
         user=os.environ["NEO4J_USERNAME"],
         password=os.environ["NEO4J_PASSWORD"],
     )
-    Logger.info("Logged to Neo4j")
+    Logger.info(msg="Logged to Neo4j")
 except Exception as e:
-    Logger.error("Failed to log in to Neo4J", msg=str(e))
+    Logger.error(msg="Failed to log in to Neo4J", error=str(e))
     raise
 
 try:
@@ -24,9 +24,9 @@ try:
     )
     channel = connection.channel()
     channel.queue_declare(queue="loader")
-    Logger.info("Connected to RabbitMQ")
+    Logger.info(msg="Connected to RabbitMQ")
 except Exception as e:
-    Logger.error("Failed to connect to RabbitMQ", msg=str(e))
+    Logger.error(msg="Failed to connect to RabbitMQ", error=str(e))
     raise
     
 def callback(ch, method, properties, body):
@@ -34,19 +34,19 @@ def callback(ch, method, properties, body):
         job = Job.Job()
         job.ParseFromString(body)
         msg = MessageToDict(job)
-        Logger.info("Message unmarshalled", message=msg)
+        Logger.info(msg="Message unmarshalled", message=msg)
     except Exception as e:
-        Logger.error("Unmarshalling message failed", message=str(e), msg=body)
+        Logger.error(msg="Unmarshalling message failed", error=str(e), message=body)
         raise
     
     try:
         DataLoader.load(neo=neo, data=msg)
-        Logger.info("Data loaded", data=msg)
+        Logger.info(msg="Data loaded", data=msg)
     except Exception as e:
-        Logger.error("Data failed to load", message=str(e), data=msg)
+        Logger.error(msg="Data failed to load", error=str(e), data=msg)
         raise
 
 channel.basic_consume(queue="loader", auto_ack=True, on_message_callback=callback)
 
-Logger.info("Waiting for messages")
+Logger.info(msg="Waiting for messages")
 channel.start_consuming()
--- back/data_reader/logger.py
+++ back/data_reader/logger.py
@@ -5,12 +5,12 @@ def logging(function_name):
     def log_decorator(function_to_log):
         @wraps(function_to_log)
         def log_decorator_wrapper(*args, **kwargs):
-            Logger.info("Begin function")
+            Logger.info(msg="Begin function")
             try:
                 value = function_to_log(*args, **kwargs)
-                Logger.info(f"{function_name} returned", args=args, kwargs=kwargs, result=value)
+                Logger.info(msg=f"{function_name} returned", args=args, kwargs=kwargs, result=value)
             except Exception as e:
-                Logger.error(f"Exception in {function_name}", msg=str(e))
+                Logger.error(msg=f"Exception in {function_name}", msg=str(e))
                 raise
             return value
         return log_decorator_wrapper
