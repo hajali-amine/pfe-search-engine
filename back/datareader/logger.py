bbf17843f4b0a20e141d325cbdc65ed1acad8d34
Amine Hajali <belhajali.amine@gmail.com>
2022-11-27 00:10:28 +0100

    fix logging and app

--- back/datareader/logger.py
+++ back/datareader/logger.py
@@ -5,15 +5,15 @@ from structlog import get_logger
 logger = get_logger("dataloader")
 
 
-def logging(function_name):
+def logging(function_name, return_meth=None):
     def log_decorator(function_to_log):
         @wraps(function_to_log)
         def log_decorator_wrapper(*args, **kwargs):
-            logger.info("Begin function {function_name}")
+            logger.info(f"Begin function {function_name}")
             try:
                 value = function_to_log(*args, **kwargs)
                 logger.info(
-                    f"{function_name} returned", args=args, kwargs=kwargs, result=value
+                    f"{function_name} returned", args=args, kwargs=kwargs, result=return_meth(value) if return_meth else value
                 )
             except Exception as e:
                 logger.error(
